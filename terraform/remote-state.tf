152005922ba4b44d69143ecbe2a889043d862091
Melvyn de Kort <melvyn@mdekort.nl>
2025-11-21 00:01:09 +0100

    Remove unused cloudsetup remote state reference

--- terraform/remote-state.tf
+++ terraform/remote-state.tf
@@ -8,16 +8,6 @@ data "terraform_remote_state" "tf_cloudflare" {
   }
 }
 
-data "terraform_remote_state" "cloudsetup" {
-  backend = "s3"
-
-  config = {
-    bucket = "mdekort.tfstate"
-    key    = "cloudsetup.tfstate"
-    region = "eu-west-1"
-  }
-}
-
 data "terraform_remote_state" "tf_grafana" {
   backend = "s3"
 
